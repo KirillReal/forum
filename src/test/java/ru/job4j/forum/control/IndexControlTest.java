0e6eee87b44bf887fafb1418a93f5344c1a393e7
KirillReal <kiril5471710@gmail.com>
2021-05-24 13:00:46 +0300

    0. Spring Liquibase [#302330]

--- src/test/java/ru/job4j/forum/control/IndexControlTest.java
+++ src/test/java/ru/job4j/forum/control/IndexControlTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.job4j.forum.Main;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import ru.job4j.forum.Main;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
